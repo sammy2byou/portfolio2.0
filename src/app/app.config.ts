cc19ed8b767e2cf1d108f46242f82964e1fd2507
Samantha McKenzie <80702255+sammy2byou@users.noreply.github.com>
2025-06-29 16:36:02 -0400

    Real test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HomeComponent } from './home/home.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
